317d2a4fccd828b6d7be953a00cc98f57fc2db73
Miguel Fialho <fialhomig@gmail.com>
2022-06-05 12:13:01 -0300

    Iniciando as rotas com o módulo Router

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -9,12 +9,13 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import localePt from '@angular/common/locales/pt';
+import { AppRoutingModule } from './app-routing.module';
 
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
